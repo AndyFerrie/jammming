7bd5ff4088b817a82c1fdb223c489a97a04a60cc
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2022-04-29 19:22:02 +0100

    fin

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,10 +10,11 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1}, {name: 'name2', artist: 'artist2', album: 'album2', id: 2}, {name: 'name3', artist: 'artist3', album: 'album3', id: 3}],
+      searchResults: [],
       playlistName: 'My Playlist',
-      playlistTracks: [{name: 'playlistName1', artist: 'playlistArtist1', album: 'PlaylistAlbum1', id: 4}, {name: 'playlistName2', artist: 'playlistArtist2', album: 'PlaylistAlbum2', id: 5}, {name: 'playlistName3', artist: 'playlistArtist3', album: 'PlaylistAlbum3', id: 6}]
+      playlistTracks: []
     };
+    
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
@@ -43,6 +44,12 @@ class App extends React.Component {
 
   savePlaylist() {
     const trackUris = this.state.playlistTracks.map(track => track.uri);
+    Spotify.savePlayList(this.state.playlistName, trackUris).then(() => {
+      this.setState({
+        playlistName: 'New Playlist',
+        playlistTracks: []
+      })
+    })
   }
 
   search(term) {
